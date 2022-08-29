27abd94ed46017fbcf4fe7d881b64443417a25ec
Dima Zaytsev <dmitry.zaicew@gmail.com>
2022-08-30 00:44:06 +0200

    Disabled LeakCanary

--- app/src/debug/java/org/mozilla/fenix/DebugFenixApplication.kt
+++ app/src/debug/java/org/mozilla/fenix/DebugFenixApplication.kt
@@ -6,33 +6,8 @@ package org.mozilla.fenix
 
 import android.os.StrictMode
 import androidx.preference.PreferenceManager
-import leakcanary.AppWatcher
-import leakcanary.LeakCanary
 import org.mozilla.fenix.ext.application
 import org.mozilla.fenix.ext.getPreferenceKey
 
 class DebugFenixApplication : FenixApplication() {
-
-    override fun setupLeakCanary() {
-        if (!AppWatcher.isInstalled) {
-            AppWatcher.manualInstall(
-                application = application,
-                watchersToInstall = AppWatcher.appDefaultWatchers(application)
-            )
-        }
-
-        val isEnabled = components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
-            PreferenceManager.getDefaultSharedPreferences(this)
-                .getBoolean(getPreferenceKey(R.string.pref_key_leakcanary), true)
-        }
-
-        updateLeakCanaryState(isEnabled)
-    }
-
-    override fun updateLeakCanaryState(isEnabled: Boolean) {
-        LeakCanary.showLeakDisplayActivityLauncherIcon(isEnabled)
-        components.strictMode.resetAfter(StrictMode.allowThreadDiskReads()) {
-            LeakCanary.config = LeakCanary.config.copy(dumpHeap = isEnabled)
-        }
-    }
 }
